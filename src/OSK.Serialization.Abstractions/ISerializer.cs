a216024174166e0f51578e2365dcc7d446d9655f
BlankDev117 <mmmasent@gmail.com>
2024-03-20 01:44:04 -0400

    Add initial abstraction layer (OSK-1) (#2)
    * Add initial abstraction layer (OSK-1)
    
    Motivation
    ----
    Need an initial project for an open source Serialization mechanism
    
    Modifications
    ----
    * Add common interface and extension that can be used across the implemented serializers
    
    Result
    ----
    Initial abstraction layer is available for use
    
    * Adjust response type

---
+++ src/OSK.Serialization.Abstractions/ISerializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OSK.Serialization.Abstractions
+{
+    public interface ISerializer
+    {
+        ValueTask<object> DeserializeAsync(byte[] data, Type type, CancellationToken cancellationToken = default);
+
+        ValueTask<byte[]> SerializeAsync(object data, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/OSK.Serialization.Abstractions/SerializerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace OSK.Serialization.Abstractions
+{
+    public static class SerializerExtensions
+    {
+        public static async ValueTask<T> DeserializeAsync<T>(this ISerializer serializer, byte[] data, CancellationToken cancellationToken = default)
+        {
+            return (T)await serializer.DeserializeAsync(data, typeof(T), cancellationToken);
+        }
+    }
+}
