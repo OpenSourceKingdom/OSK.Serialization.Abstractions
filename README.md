3223beae1830e10f9f254292852d98db592f2436
M <mmmasent@gmail.com>
2024-11-03 18:44:35 -0500

    Add better documentation (OSK-11)
    Motivation
    ----
    Better documentation for users
    
    Modifications
    ----
    * added better readme explanation
    
    Result
    ----
    Clearer README to the usage of the library

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # OSK.Serialization.Abstractions
-Abstraction layer for serialization of objects within .NET
+An abstraction layer for serialization of objects within .NET.
+
+# OSK.Serialization.Abstractions.Binary
+An abstraction layer for binary serialization of objects within .NET.
+
+# OSK.Serialization.Abstractions.Json
+An abstraction layer for JSON serialization of objects within .NET.
+
+# OSK.Serialization.Abstractions.Yaml
+An abstraction layer for yaml serialization of objects within .NET.
+
+# Usage
+This library is meant to allow for integration with the serialization mechanism used within OSK projects and does not provide a direct implementation, but rather provides the abstraction objects that are expected to be defined in implementations of OSK style serializers.
+
+# Issues
+Please provide any issues using the github issue tracker
+
+# Contributions
+Contributions are encouraged, but should utilize the branch naming pattern `OSK-{issue #}-{insert}-{branch}-{name}` where the issue # is tied to an actual repository issue.
--- src/OSK.Serialization.Abstractions/ISerializer.cs
+++ src/OSK.Serialization.Abstractions/ISerializer.cs
@@ -6,8 +6,21 @@ namespace OSK.Serialization.Abstractions
 {
     public interface ISerializer
     {
+        /// <summary>
+        /// Deserializes a byte array of data into an object using the specified type.
+        /// </summary>
+        /// <param name="data">The byte array where the data is stored.</param>
+        /// <param name="type">The expected type of the deserialized object.</param>
+        /// <param name="cancellationToken">The token to cancel the operation</param>
+        /// <returns>The object deserialized into the expected typed.</returns>
         ValueTask<object> DeserializeAsync(byte[] data, Type type, CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// Serializes an object into an array of bytes for storage.
+        /// </summary>
+        /// <param name="data">The object being serialized.</param>
+        /// <param name="cancellationToken">The token to cancel the operation.</param>
+        /// <returns>The byte representation of the serialized object.</returns>
         ValueTask<byte[]> SerializeAsync(object data, CancellationToken cancellationToken = default);
     }
 }
